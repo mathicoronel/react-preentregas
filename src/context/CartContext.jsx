47a6cbb5e20d55a195ebd7d29b235b1a7e084c79
Mathias Coronel <mathicoronel1@gmail.com>
2024-01-22 21:41:46 -0300

    agrego funciones para utilizar en los children del context

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -19,6 +19,7 @@ export const CartContextProvider = ({children}) => {
             newCart.push(addedItem)
         }
         setCart(newCart)
+        console.log(newCart)
     }
 
     const productsQuantity = () => {
