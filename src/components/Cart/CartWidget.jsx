36ede9df05f3d42bac6b91cd52c27c53176154f5
Mathias Coronel <mathicoronel1@gmail.com>
2024-01-22 21:44:44 -0300

    agrego la función para el número de la cantidad de productos en el carrito

--- src/components/Cart/CartWidget.jsx
+++ src/components/Cart/CartWidget.jsx
@@ -1,16 +1,20 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Link } from 'react-router-dom'
+import CartContext from '../../context/CartContext'
 
 const CartWidget = () => {
+
+  const {productsQuantity} = useContext(CartContext)
+
   return (
     <div className='cart-container'>
-      <Link to={"./cart"}>
+      <Link to={"/cart"}>
         <button className='btn-cart'>
           <span className="material-symbols-outlined">
             shopping_cart
           </span>
           <span className='numero-carrito'>
-            0
+            {productsQuantity()}
           </span>
         </button>
       </Link>
