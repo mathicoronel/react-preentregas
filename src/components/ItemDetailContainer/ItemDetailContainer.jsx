f00f258b356c0c0c3afda1043e5576901201690a
Mathias Coronel <mathicoronel1@gmail.com>
2024-01-26 11:00:30 -0300

    añado timeout para la carga de cada producto al entrar a su detalle

--- src/components/ItemDetailContainer/ItemDetailContainer.jsx
+++ src/components/ItemDetailContainer/ItemDetailContainer.jsx
@@ -2,22 +2,33 @@ import React, { useEffect, useState } from 'react'
 import ItemDetail from './ItemDetail'
 import { useParams } from 'react-router-dom'
 import {doc, getDoc, getFirestore} from "firebase/firestore"
+import Loader from '../Loader/Loader'
 
 const ItemDetailContainer = () => {
     const {id} = useParams()
     const [product, setProduct] = useState([])
 
+    const [loading, setLoading] = useState(true)
+
     useEffect(() => {
         const db = getFirestore()
-
         const oneItem = doc(db, "productos", `${id}`)
-        getDoc(oneItem).then((snapshot) => {
-            if(snapshot.exists()){
-                const doc = {...snapshot.data()}
-                setProduct(doc)
-            }
-        })
+
+        setTimeout(() => {
+            getDoc(oneItem).then((snapshot) => {
+                if(snapshot.exists()){
+                    const doc = {...snapshot.data()}
+                    setProduct(doc)
+                    setLoading(false)
+                }
+            })
+        }, 1000);
+
     }, [])
+
+    if(loading) {
+        return <Loader />
+    }
 
     return (
     <div>
