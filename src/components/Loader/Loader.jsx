56007d1af039d03fbcb3a24c26232bc361f8128a
Mathias Coronel <mathicoronel1@gmail.com>
2024-01-26 18:26:50 -0300

    cambio el spinner de carga

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 
 const Loader = () => {
     return (
-      <div className='spinner'>
-        <div class="spinner-grow m-5" role="status">
-          <span class="visually-hidden">Loading...</span>
+        <div className='spinner'>
+            <div class="spinner-grow m-5" role="status">
+                <span class="visually-hidden">Loading...</span>
+            </div>
         </div>
-      </div>
     );
 }
 
