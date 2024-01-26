3f1b4d0d106dff5ade480777e8e53f05a922c07e
Mathias Coronel <mathicoronel1@gmail.com>
2024-01-26 11:01:03 -0300

    agrego timeout para la carga de todos los productos

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -2,24 +2,35 @@ import React, { useEffect, useState } from 'react'
 import ItemList from './ItemList'
 import { useParams } from 'react-router-dom'
 import { collection, getDocs, getFirestore } from 'firebase/firestore'
+import Loader from '../Loader/Loader'
 
 const ItemListContainer = () => {
     const {categoryId} = useParams()
 
     const [products, setProducts] = useState([])
+
+    const [loading, setLoading] = useState(true)
     
     useEffect(() => {
         const db = getFirestore()
         const itemsCollection = collection(db, "productos")
 
-        getDocs(itemsCollection).then((snapshot) => {
-            const docs = snapshot.docs.map((doc) => ({
-                ...doc.data(),
-                id: doc.id,
-            }));
-        setProducts(docs)
-        })
+        setTimeout(() => {
+            getDocs(itemsCollection).then((snapshot) => {
+                const docs = snapshot.docs.map((doc) => ({
+                    ...doc.data(),
+                    id: doc.id,
+                }));
+            setProducts(docs)
+            setLoading(false)
+            })
+        }, 2500);
+
     }, [])
+
+    if(loading) {
+        return <Loader />
+    }
 
     const filteredProducts = products.filter((p) => p.categoria === categoryId)
 
